a1638c00ce8ff574cff16ccc14752ea4dbd2a660
itlijunjie <itlijunjie@gmail.com>
2015-12-05 22:02:09 +0800

    modify 添加颜色代码

--- CommonUtil/CommonUtil/QuickUIUtil.swift
+++ CommonUtil/CommonUtil/QuickUIUtil.swift
@@ -20,55 +20,24 @@ public class QuickUIUtil {
         return Static.instance!
     }
     
-//    class var shared: QuickUIUtil {
-//        struct Static {
-//            static var onceToken: dispatch_once_t = 0
-//            static var instance: QuickUIUtil? = nil
-//        }
-//        dispatch_once(&Static.onceToken) {
-//            Static.instance = QuickUIUtil()
-//        }
-//        return Static.instance!
-//    }
+    public func colorFromRed(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return self.colorFromRed(red, green: green, blue: blue, alpha: 1.0);
+    }
     
+    public func colorFromRed(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+        return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha);
+    }
     
-//    + (UIColor *)colorFromRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue
-//    {
-//    return [CUCFQuickUIUitil colorFromRed:red green:green blue:blue withAlpha:1.0];
-//    }
-//    
-//    + (UIColor *)colorFromRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue withAlpha:(CGFloat)alpha
-//    {
-//    return [UIColor colorWithRed:red/255.0 green:green/255.0 blue:blue/255.0 alpha:alpha];
-//    }
-//    
-//    + (UIColor *)colorFromHexString:(NSString *)hexString
-//    {
-//    if (CUCFStringIsNull(hexString)) {
-//    return nil;
-//    }
-//    
-//    unsigned hexNum;
-//    if ( ![[NSScanner scannerWithString:hexString] scanHexInt:&hexNum] ) {
-//    return nil;
-//    }
-//    
-//    return [UIColor colorWithRed:((float)((hexNum & 0xFF0000) >> 16))/255.0 green:((float)((hexNum & 0xFF00) >> 8))/255.0 blue:((float)(hexNum & 0xFF))/255.0 alpha:1.0];
-//    }
-//    + (UIColor *)colorFromHexString:(NSString *)hexString alpha:(CGFloat)alpha;
-//    {
-//    if (CUCFStringIsNull(hexString)) {
-//    return nil;
-//    }
-//    
-//    unsigned hexNum;
-//    if ( ![[NSScanner scannerWithString:hexString] scanHexInt:&hexNum] ) {
-//    return nil;
-//    }
-//    
-//    return [UIColor colorWithRed:((float)((hexNum & 0xFF0000) >> 16))/255.0 green:((float)((hexNum & 0xFF00) >> 8))/255.0 blue:((float)(hexNum & 0xFF))/255.0 alpha:alpha];
-//    
-//    }
+    public func colorFromHexString(hexString: String) -> UIColor {
+        return self.colorFromHexString(hexString, alpha: 1.0);
+    }
+    
+    public func colorFromHexString(hexString: String, alpha: CGFloat) -> UIColor {
+        var hexNum: UInt32 = 0;
+        NSScanner(string: hexString).scanHexInt(&hexNum);
+        return UIColor(red: ((CGFloat)((hexNum & 0xFF0000) >> 16))/255.0 , green: ((CGFloat)((hexNum & 0xFF00) >> 8))/255.0, blue: ((CGFloat)(hexNum & 0xFF))/255.0, alpha: alpha);
+    }
+    
 //    
 //    + (CGContextRef)getTextCTMContextRefFromView:(UIView *)aView
 //    {
